c248556bdfddc262f1eaee4d20c18da2540cb7d6
keerthana <keerthana.m@iamneo.ai>
2025-08-06 17:29:30 +0530

    mhc-queue

--- src/api/mhc-checkin/mhc-checkin.controller.ts
+++ src/api/mhc-checkin/mhc-checkin.controller.ts
@@ -194,7 +194,7 @@ export async function completeCheckIn(req: Request, res: Response) {
         if (thresholds.includes(count)) {
             // const alreadySent = await wasThresholdSentRecently(count);
             // const phoneNumbers = ['919342287945', '917708059010', '916382348092'];
-            const phoneNumbers =['919620306613', '916364833988', '919880544866']
+            const phoneNumbers =['919620306613', '916364833988', '919880544866'];
 
             await Promise.all(
                 phoneNumbers.map(num => sendWhatsAppNotification(num, count))
